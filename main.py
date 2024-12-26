28eced242f5439e1e2ea74dd7037203b2c4f5ddb
Vanshaj Vohra <vanshajvohra@gmail.com>
2024-12-25 23:44:50 -0500

    Model validation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
 import pandas as pd
 
 melb_data_path = 'melb_data.csv'
@@ -22,5 +24,15 @@ X = melb_data[melb_features]
 melb_model = DecisionTreeRegressor(random_state=1) # choosing a number means we get the same results in every run
 melb_model.fit(X, y) # Fitting features and target
 
-print("Initial Predictions")
-print(melb_model.predict(X.head()))
+predicted_prices = melb_model.predict(X)
+# print("Initial Predictions")
+# print(predicted_prices)
+
+mae = mean_absolute_error(y, predicted_prices) # Measure of model quality
+print(mae)
+
+train_x, val_x, train_y, val_y = train_test_split(X, y, random_state=0) # splitting data into training and validation sets
+new_model = DecisionTreeRegressor(random_state=0)
+new_model.fit(train_x, train_y) # Fitting or training is always done on training data
+new_predictions = new_model.predict(val_x) # Predictions are done on validation data
+print(mean_absolute_error(val_y, new_predictions)) # predictions are compared with unseen data
